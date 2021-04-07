f7f7547ea55a2ae89ca08f6799c69702bdbb2d33
robert <robert@mottus.se>
2021-04-07 15:39:53 +0200

    2020 Day15: refactor

--- src/year2020/day15/part2.clj
+++ src/year2020/day15/part2.clj
@@ -1,33 +1,42 @@
 (ns year2020.day15.part2
   (:require [ysera.test :refer [is is= is-not]]))
 
-(declare init-state)
+; https://adventofcode.com/2020/day/15
+
+;In this game, the players take turns saying numbers. They begin by taking turns reading from a list of starting numbers (your puzzle input). Then, each turn consists of considering the most recently spoken number:
+;If that was the first time the number has been spoken, the current player says 0.
+;Otherwise, the number had been spoken before; the current player announces how many turns apart the number is from when it was previously spoken.
+;So, after the starting numbers, each turn results in that player speaking aloud either 0 (if the last number is new) or an age (if the last number is a repeat).
+
+(declare numbers->state)
 (declare iterate-map)
 
+(comment
+  (time (get-number 2020 [1, 17, 0, 10, 18, 11, 6] ))    ; 595
+  (time (get-number 30000000 [1, 17, 0, 10, 18, 11, 6])) ; 1708310
+  (time (get-number 30000000 [14,8,16,0,1,17]))          ; 505
+  )
+
 (defn get-number
-  {:test (fn []
+  {:doc "Returns number at 'pos', given 'init-numbers'"
+   :test (fn []
            (is= (get-number 4 [1, 2, 3]) 0)
-           (is= (get-number 30000000 [1, 17, 0, 10, 18, 11, 6]) 1708310)
+           ;(is= (get-number 30000000 [1, 17, 0, 10, 18, 11, 6]) 1708310)
            )}
   [pos init-numbers]
-  (let
-    [state (init-state init-numbers)
-     ;old (into {} (map-indexed (fn [i n] [n (inc i)]) (drop-last init-numbers)))
-     ]
-    (loop [s state]
-      (if (== (:last-pos s) pos)
-        (:last-num s)
-        (recur (iterate-map s))
-        )
+  (loop [{last-pos :last-pos last-num :last-num :as state} (numbers->state init-numbers)]
+    (if (== last-pos pos)
+      last-num
+      (recur (iterate-map state))
       )
     )
   )
 
-(defn init-state
+(defn numbers->state
   {:doc  "Initialzes position-map"
    :test (fn []
-           (is= (init-state [5 6 7]) {:last-num 7 :last-pos 3 :pos-map {5 1 6 2}})
-           (is= (init-state [1 2 2]) {:last-num 2 :last-pos 3 :pos-map {1 1 2 2}})
+           (is= (numbers->state [5 6 7]) {:last-num 7 :last-pos 3 :pos-map {5 1 6 2}})
+           (is= (numbers->state [1 2 2]) {:last-num 2 :last-pos 3 :pos-map {1 1 2 2}})
            )}
   [init-numbers]
   (let
@@ -39,23 +48,15 @@
     ))
 
 (defn iterate-map
-  {:doc  "Initialzes position-map"
+  {:doc  "Adds one iteration to state"
    :test (fn []
-           (is= (iterate-map {:last-num 0 :last-pos 4 :pos-map {1 1 2 2 3 3}})
-                {:last-num 0 :last-pos 5 :pos-map {1 1 2 2 3 3 0 4}})
+           (is= (iterate-map (numbers->state [1 2 3])) (numbers->state [1 2 3 0]))
+           (is= (iterate-map (numbers->state [1 2 3 0])) (numbers->state [1 2 3 0 0]))
+           (is= (iterate-map (numbers->state [1 2 3 0 0])) (numbers->state [1 2 3 0 0 1]))
            )}
-  [state]
-  (let
-    [num-pos (:pos-map state)
-     last-number (:last-num state)
-     last-pos (:last-pos state)
-     new-number (if (contains? num-pos last-number) (- last-pos (num-pos last-number)) 0)
-     new-last-pos (inc last-pos)
-     new-num-pos (assoc num-pos last-number last-pos)
-     new-state {:last-num new-number :last-pos new-last-pos :pos-map new-num-pos}
-     ]
-    ;(println "old:" state)
-    ;(println "new:" new-state)
-    new-state
-    ))
+  [{num-pos :pos-map last-number :last-num last-pos :last-pos}]
+   {:last-num (if-let [prev-pos (num-pos last-number)] (- last-pos prev-pos) 0)
+    :last-pos (inc last-pos)
+    :pos-map  (assoc num-pos last-number last-pos)}
+   )
 
